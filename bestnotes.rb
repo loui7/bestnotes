82f472a199a0b321019a125fca2b8b6c92d86942
absmohamed <cabdullahidaud@Hotmail.com>
2019-04-23 14:30:15 +1000

    Added ability to add and view notes within categories

--- bestnotes.rb
+++ bestnotes.rb
@@ -1,10 +1,12 @@
 require_relative "./classes/Category"
+require_relative "./classes/Note"
 
 categories = []
 
 puts "Hello, Welcome to BestNotes"
 
 loop do
+
     
     if(categories.length > 0)
         puts "These are your current categories:"
@@ -16,16 +18,39 @@ loop do
     category_name = gets.strip
 
     category_index_if_exists = categories.find_index {|category| category.name == category_name}
+
     if(category_index_if_exists != nil)
         selected_category = categories[category_index_if_exists]
     else
-        categories.push(Category.new(categories.length + 1, category_name))
+        new_category_id = categories.length + 1
+        categories.push(Category.new(new_category_id, category_name))
         selected_category = categories[categories.length - 1]
-    end
-    
+        category_index_if_exists = categories.length - 1
+    end    
+
     puts "You have selected category #{selected_category.name}"
 
-    
+    until selected_category == nil
+        if(selected_category.notes.length == 0)
+            selected_category.add_note            
+        end
 
+        puts "These are your current notes: "
+        selected_category.notes.each {|note| puts "#{note.id}. #{note.contents}" }
+
+        puts "You can input (n) to add a new note or (m) to return to the previous menu."
+
+        notes_menu_entry = gets.strip
+
+        if(notes_menu_entry == "n")
+            selected_category.add_note
+        elsif(notes_menu_entry == "m")
+            categories[category_index_if_exists] = selected_category
+            selected_category = nil
+        else
+            puts "#{notes_menu_entry} is an invalid option, please try again."
+        end
+
+    end
 end
 
--- classes/Category.rb
+++ classes/Category.rb
@@ -7,4 +7,12 @@ class Category
             @notes = []
     end
 
+    def add_note()
+        puts "Enter note: "
+        new_note_id = @notes.length + 1
+        new_note_contents = gets.chomp
+        new_note = Note.new(new_note_id, new_note_contents)
+        @notes.push(new_note)
+    end
+
 end
