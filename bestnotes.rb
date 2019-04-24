871d6cf6b7532573785b1658caec0df381edcd36
Louis Lloyd-Besson <loui7lb@gmail.com>
2019-04-24 21:30:52 +1000

    massively improved modularisation

--- bestnotes.rb
+++ bestnotes.rb
@@ -4,80 +4,102 @@ require_relative "./classes/User"
 
 require "yaml"
 
-storage = "./storage.yml"
-
-if File.exist?(storage)
-    users = YAML.safe_load(File.read(storage), [User, Category, Note, Time])
-else
-    users = []
-    File.open(storage, "w+") { |file| file.write(users.to_yaml) }
-end
-
-puts "Hello, Welcome to BestNotes"
-
-user_index = nil
-selected_user = nil
-
-loop do
-    while selected_user.nil?
-        if users.empty?
-            puts "No accounts found!"
-        else
-            puts "Press (l) to login, (r) to register or (q) to quit."
-            auth_menu_entry = gets.strip.downcase
-        end
-
-        # If there are no users OR the user specifically wants to add an account. Creates new user account.
-        if users.empty? || auth_menu_entry == "r"
-            puts "Please enter a username: "
-            new_username = gets.strip
-
-            new_user_id = users.length + 1
-
-            puts "Do you want to set a password? (y/n) or (m) to return to the main login screen."
-            password_wanted = gets.strip.downcase
-
-            new_user = nil
-
-            while new_user.nil?
-                if password_wanted == "y"
-                    puts "Enter password: "
-                    new_password = gets.chomp
-                    new_user = User.new(new_user_id, new_username, new_password)
-                    puts "Please confirm your password."
-                elsif password_wanted == "n"
-                    new_user = User.new(new_user_id, new_username)
-
-                end
-
-            end
-
-            users.push(new_user)
-            user_index = users.length - 1
-            # TODO: make user confirm password? Find way to avoid it being possible to access object without password.
-            selected_user = users[user_index].auth
-        elsif auth_menu_entry == "l"
-            puts "Username: "
-            entered_username = gets.strip
-            user_index = users.find_index { |user| user.username == entered_username }
-            if user_index.nil?
-                puts "That username was not found! please try again"
-            else
-                puts "Password: "
-                selected_user = users[user_index].auth
-            end
-        elsif auth_menu_entry == "q"
-            File.open(storage, "r+") do |f|
-                f.write(users.to_yaml)
-            end
-            return
-        else
-            puts "#{auth_menu_entry} is an invalid option, please try again."
-        end
+def main_dialogue(storage)
+    if File.exist?(storage)
+        users = YAML.safe_load(File.read(storage), [User, Category, Note, Time])
+    else
+        users = []
+        File.open(storage, "w+") { |file| file.write(users.to_yaml) }
     end
 
-    until selected_user.nil?
+    puts "Hello, Welcome to BestNotes"
 
+    users = users_dialogue(users)
+
+    File.open(storage, "r+") do |f|
+        f.write(users.to_yaml)
+    end
+end
+
+def users_dialogue(users)
+    selected_user = nil
+
+    loop do
+        while selected_user.nil?
+            if users.empty?
+                puts "No accounts found!"
+            else
+                puts "Press (l) to login, (r) to register or (q) to quit."
+                auth_menu_entry = gets.strip.downcase
+            end
+
+            if users.empty? || auth_menu_entry == "r"
+                new_user = add_user(users.length + 1)
+                users.push(new_user)
+                user_index = users.length - 1
+                selected_user = users[user_index].auth
+            elsif auth_menu_entry == "l"
+                user_index = login_dialogue(users)
+                if user_index.nil?
+                    puts "Sorry, that username does not exist in our database. Please try again."
+                else
+                    selected_user = users[user_index].auth
+                end
+            elsif auth_menu_entry == "q"
+                return users
+            else
+                puts "#{auth_menu_entry} is an invalid option, please try again."
+            end
+        end
+
+        selected_user = categories_dialogue(selected_user)
+
+        users[user_index] = selected_user
+        selected_user = nil
+    end
+end
+
+def add_user(new_user_id)
+    puts "Please enter a username: "
+    new_username = gets.strip
+
+    puts "Do you want to set a password? (y/n) or (m) to return to the main login screen."
+    password_wanted = gets.strip.downcase
+
+    new_user = nil
+
+    while new_user.nil?
+        if password_wanted == "y"
+            puts "Enter password: "
+            new_password = gets.chomp
+            new_user = User.new(new_user_id, new_username, new_password)
+            puts "Please confirm your password."
+        elsif password_wanted == "n"
+            new_user = User.new(new_user_id, new_username)
+        elsif password_wanted == "m"
+            return nil
+        end
+
+    end
+
+    return new_user
+end
+
+def login_dialogue(users)
+    puts "Username: "
+    entered_username = gets.strip
+    user_index = users.find_index { |user| user.username == entered_username }
+    if user_index.nil?
+        puts "That username was not found! please try again"
+        return nil
+    else
+        puts "Password: "
+        return user_index
+    end
+end
+
+def categories_dialogue(selected_user)
+    loop do
         if selected_user.categories.empty?
             puts "No categories found! Please press (n) to add a new category or (m) to return to the login screen."
         else
@@ -93,12 +115,11 @@ loop do
         elsif category_menu_entry == "n"
             selected_user.add_category
         elsif category_menu_entry == "m"
-            users[user_index] = selected_user
-            user_index = nil
-            selected_user = nil
+            return selected_user
         else
             puts "#{category_menu_entry} is an invalid option, please try again."
         end
-
     end
 end
+
+main_dialogue("./storage.yml")
